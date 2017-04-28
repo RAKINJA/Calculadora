11051f36f8f07e8af73e533cca5b9f537f2e71f0
RAKINJA <atlante_infinito@outlook.com>
2017-04-27 20:54:35 -0500

    Add files via upload

---
+++ Calculadora/src/calculadora/TestCalculadora.java
@@ -0,0 +1,15 @@
+/*
+ * GNU GPL
+ */
+package calculadora;
+
+/**
+ * @author Rakinja
+ */
+public class TestCalculadora {
+    
+    public static void main ( String [] args ){
+        Calculadora c1 = new Calculadora();
+    }
+    
+}
